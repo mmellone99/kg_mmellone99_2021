12eef8a2c3a09dd9efeb276fb654f4a8aab873b6
Mike <mmellone99@gmail.com>
2020-03-15 20:14:25 -0400

    wrote function for one-to-one char mapping

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
 #Kargo Software Engineer Intern Assesment
 #Michael Mellone
+
+import sys
+
+def charMap(*argv):
+    if len(sys.argv[1]) == len(sys.argv[2]):  
+        dict = {}   
+        for i in range(len(sys.argv[1])):  
+            if sys.argv[1][i] not in dict:
+                dict[sys.argv[1][i]] = sys.argv[2][i]
+            if dict[sys.argv[1][i]] != sys.argv[2][i]:
+                return False
+        return True
+    return False  
+
+
+print(charMap())
+
